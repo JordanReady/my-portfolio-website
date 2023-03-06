8b05b4e84bb3d5d3f97a138949861a5ae70f23a1
JordanReady <jready112@gmail.com>
2023-03-06 01:15:35 -0600

    solved ts errors in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,6 +9,10 @@ interface NavbarProps {
   handleIndex: (index: number) => void;
 }
 
+interface ButtonStyle extends React.CSSProperties {
+  "--i": string;
+}
+
 export default function Navbar(props: NavbarProps) {
   const { activeIndex, handleIndex } = props;
 
@@ -20,35 +24,35 @@ export default function Navbar(props: NavbarProps) {
       </a>
       <nav className="navbar">
         <button
-          style={{ "--i": "1" }}
+          style={{ "--i": "1" } as ButtonStyle}
           className={activeIndex === 0 ? "active" : ""}
           onClick={() => handleIndex(0)}
         >
           <i className="bx bx-home-smile"></i> Home
         </button>
         <button
-          style={{ "--i": "2" }}
+          style={{ "--i": "2" } as ButtonStyle}
           className={activeIndex === 1 ? "active" : ""}
           onClick={() => handleIndex(1)}
         >
           <i className="bx bx-info-circle"></i> About
         </button>
         <button
-          style={{ "--i": "3" }}
+          style={{ "--i": "3" } as ButtonStyle}
           className={activeIndex === 2 ? "active" : ""}
           onClick={() => handleIndex(2)}
         >
           <i className="bx bxs-graduation"></i> Skills
         </button>
         <button
-          style={{ "--i": "4" }}
+          style={{ "--i": "4" } as ButtonStyle}
           className={activeIndex === 3 ? "active" : ""}
           onClick={() => handleIndex(3)}
         >
           <i className="bx bx-code-alt"></i> Projects
         </button>
         <button
-          style={{ "--i": "5" }}
+          style={{ "--i": "5" } as ButtonStyle}
           className={activeIndex === 4 ? "active" : ""}
           onClick={() => handleIndex(4)}
         >
