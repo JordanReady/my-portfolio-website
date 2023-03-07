8068b00e6d5da314d5c15f4c8c02a4f3a0be271b
JordanReady <jready112@gmail.com>
2023-03-07 03:13:43 -0600

    created about section/ cleaned assets folder

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,10 +1,55 @@
-import React, { useRef, useEffect } from "react";
-import AboutImage from "../../assets/rock-climbing-tp.png";
+import React, { useRef, useEffect, useState } from "react";
+import AboutImage from "../../assets/rock-climbing-tp-2.png";
+import AboutImage2 from "../../assets/hiking-tp.png";
+import AboutImage3 from "../../assets/gaming-tp.png";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Typed from "typed.js";
 
 import "./About.css";
 
+interface TitleStyle extends React.CSSProperties {
+  "--i": string;
+}
+
 export default function About() {
+  const typedRef = useRef<Typed | null>(null);
+  const [image, setImage] = useState(AboutImage);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      switch (image) {
+        case AboutImage:
+          setImage(AboutImage2);
+          break;
+        case AboutImage2:
+          setImage(AboutImage3);
+          break;
+        case AboutImage3:
+          setImage(AboutImage);
+          break;
+        default:
+          setImage(AboutImage);
+      }
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [image]);
+
+  useEffect(() => {
+    typedRef.current = new Typed(".typed", {
+      strings: [
+        "If you've stumbled upon this page, chances are you're curious about who built it. I'm the guy behind the magic, a fullstack web developer with a passion for creating beautiful, functional, and user-friendly web applications. When I'm not busy typing away, I'm out conquering the great outdoors. Despite my fear of heights, I find myself drawn to the thrill of rock climbing. The physical and mental challenges keep me on my toes, literally! I also enjoy hiking, golfing, and shredding the slopes on my snowboard during the winter months. Of course, I can't neglect my virtual adventures, and you'll often find me gaming with friends in my downtime. But no matter what I'm doing, I always try to keep learning and growing, especially when it comes to web development. I'm constantly seeking new resources and experimenting with the latest tools and technologies to stay at the forefront of my field. I hope this gives you a better understanding of who I am and what I'm all about. If you're interested in working together on a project, or just want to chat about web development, don't hesitate to reach out! There's a handy contact section right at the top of the page, waiting for you.",
+      ],
+      typeSpeed: 5,
+      loop: false,
+      cursorChar: "",
+    });
+
+    return () => {
+      typedRef.current?.destroy();
+    };
+  }, []);
+
   return (
     <div className="container about-container">
       <div id="triangle-bottomright"></div>
@@ -19,15 +64,20 @@ export default function About() {
             <div className="about-img">
               <img
                 className="img-fluid"
-                src={AboutImage}
+                src={image}
                 alt="rock climbing image"
               />
             </div>
           </div>
           <div className="col-md-7 about-content">
             <div className="about-content">
-              <h1 className="about-title">About Me</h1>
-              <p>
+              <h1 className="about-title">
+                <span style={{ "--i": "1" } as TitleStyle}>Who</span>
+                <span style={{ "--i": "2" } as TitleStyle}> am</span>
+                <span style={{ "--i": "3" } as TitleStyle}> I</span>
+                <span style={{ "--i": "4" } as TitleStyle}>?</span>
+              </h1>
+              <p className="typed">
                 If you've stumbled upon this page, chances are you're curious
                 about who built it. I'm the guy behind the magic, a fullstack
                 web developer with a passion for creating beautiful, functional,
