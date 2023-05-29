2b9877d8a65b9ad1c3605bf58871bcec439a97fd
JordanReady <jready112@gmail.com>
2023-05-28 19:08:13 -0500

    base url change debugg

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portfolio-website/",
+  base: "/my-portfolio-website",
   build: {
     rollupOptions: {
       output: {
